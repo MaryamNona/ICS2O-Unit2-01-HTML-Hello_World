339eaf1f609cde191b316b678be812cb582a5fd5
MaryamNona <>
2021-05-05 02:28:41 +0000

    added html file

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by: Maryam Nona
+// Created on: May 2021
+// This file contains the JS functions for index.html
+
+function myButtonClicked () {
+  //this function displays "Hello, world!"
+
+  document.getElementById('hello-world').innerHTML = 'Hello, World!'
+}
